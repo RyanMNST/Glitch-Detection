783889b10a803a2f527ec20084c08f631351f2fa
Ryan Tomelden <RyanMNST@gmail.com>
2021-07-26 16:03:17 +0800

    Test file upload

--- StreamlitDeploy.py
+++ StreamlitDeploy.py
@@ -1,13 +1,43 @@
 # Dependencies and Libraries
 from fastai.vision.all import *
 from fastai.vision.widgets import *
-import streamlit as st
+from io import BytesIO, StringIO
 
-def app():  
-    st.write("""
-    # Video Conference Glitch Detection
-    Task Item #5 Project - Convolutional Neural Network
-    """)
+import os
+import sys
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title=None,
+    page_icon=None, 
+    layout='wide', 
+    initial_sidebar_state='auto')
+
+class FileUpload(object):
+    def __init__(self):
+        self.fileTypes = ['png', 'jpg']
+
+    def app():  
+        st.write("""
+        # Video Conference Glitch Detection
+        Task Item #5 Project - Convolutional Neural Network
+        """)
+
+        st.markdown(STYLE, unsafe_allow_html=True)
+        file = st.file_uploader("Upload image file", type=['png', 'jpg'])
+
+        if not file:
+            show_file.info("Please upload an image file : {}".format(' '.join(['png', 'jpg'])))
+        content__ = file.getvalue()
+
+        if isinstance(file, BytesIO):
+            show_file.image(file)
+        else:
+            df = pd.read_csv(file)
+            st.dataframe(df.head(2))
+        file.close()
 
 if __name__ == "__main__":
-  app()
+    app = FileUpload()
+    FileUpload.app()
