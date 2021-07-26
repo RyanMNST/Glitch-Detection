7916f051fb22a0b094517933ca621ab478aac6ed
Ryan Tomelden <RyanMNST@gmail.com>
2021-07-26 15:33:08 +0800

    Test Streamlit Deploy

---
+++ StreamlitDeploy.py
@@ -0,0 +1,10 @@
+# Dependencies and Libraries
+from fastai.vision.all import *
+from fastai.vision.widgets import *
+import streamlit as st
+
+def app():  
+    st.write("""
+    # Video Conference Glitch Detection
+    Task Item #5 Project - Convolutional Neural Network
+    """)
